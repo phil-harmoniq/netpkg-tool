4075da0dfb56d5c37a859afe2958ff9c9457e3ec
Phil H <ph-hawkins@outlook.com>
2017-05-10 05:28:07 -0400

    Add link to github in help menu

--- NET_Pkg.Template/usr/bin/help-menu.sh
+++ NET_Pkg.Template/usr/bin/help-menu.sh
@@ -8,14 +8,19 @@ echo -n "--------------------- ${cyan:-}"
 echo -n "${bold:-}NET_Pkg $PKG_VERSION"
 echo "${normal:-} ---------------------"
 echo
+echo "          ${bold:-}${cyan:-}Info:${normal:-}"
 echo "           App: $DLL_NAME.dll"
 echo "            OS: $OS_PNAME"
 echo " .NET location: $NET"
 echo
-echo "        Optional Arguments:"
+echo "        ${bold:-}${cyan:-}Optional Arguments:${normal:-}"
 echo " --npk-verbose or --npk-v : Verbose output"
 echo "    --npk-help or --npk-h : Help menu (this page)"
 echo "     --npk-dir or --npk-d : View location of .NET runtime"
+echo
+echo "   More information & source code available on github:"
+echo "   https://github.com/phil-harmoniq/NET_Pkg"
+echo "   Copyright (c) 2017 - MIT License"
 echo
 echo "---------------------------------------------------------"
 echo
--- tools/pkg-tool-help.sh
+++ tools/pkg-tool-help.sh
@@ -8,16 +8,21 @@ echo -n "------------------ ${cyan:-}"
 echo -n "${bold:-}NET_Pkg.Tool $PKG_VERSION"
 echo "${normal:-} -------------------"
 echo
+echo "          ${bold:-}${cyan:-}Info:${normal:-}"
 echo "            OS: $OS_PNAME"
 echo " .NET location: $NET"
 echo
-echo "   Usage:"
+echo "   ${bold:-}${cyan:-}Usage:${normal:-}"
 echo "  ./NET_Pkg.Tool [.NET Project] [Destination] [flags]"
 echo
-echo "   Optional Arguments:"
+echo "   ${bold:-}${cyan:-}Optional Arguments:${normal:-}"
 echo "      -verbose or -v : Verbose output"
 echo "         -help or -h : Help menu (this page)"
 echo "          -dir or -d : View location of .NET runtime"
+echo
+echo "   More information & source code available on github:"
+echo "   https://github.com/phil-harmoniq/NET_Pkg"
+echo "   Copyright (c) 2017 - MIT License"
 echo
 echo "---------------------------------------------------------"
 echo
