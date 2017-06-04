40ac6cda45fc6242e5ac08afefad6004d1e560f5
Phil H <ph-hawkins@outlook.com>
2017-06-04 18:22:20 -0400

    Docker else catch not needed

--- build.sh
+++ build.sh
@@ -119,8 +119,6 @@ copy_files() {
         mv ./squashfs-root ./appimagetool
         ln -s /tmp/.netpkg-tool/usr/share/appimagetool/AppRun /tmp/.netpkg-tool/usr/bin/appimagetool
         cd $PKG_DIR
-    else
-        ln /tmp/.netpkg-tool/usr/share/appimagetool/AppRun /usr/local/bin/appimagetool
     fi
 
     chmod +x /tmp/.netpkg-tool/AppRun
