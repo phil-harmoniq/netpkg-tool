e3e8531a92f7218b029d32da69dc976cfa5fd583
Phil H <ph-hawkins@outlook.com>
2017-07-28 14:19:30 -0400

    Simplified build process

--- build-prep.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-export HERE=$(dirname "$(readlink -f "${0}")")
-rm -rf $HERE/squashfs-root $HERE/appimagetool.AppImage
-wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O $HERE/appimagetool.AppImage
-chmod a+x $HERE/appimagetool.AppImage
-$HERE/appimagetool.AppImage --appimage-extract
-mv $HERE/squashfs-root $HERE/appimagetool
-rm $HERE/appimagetool.AppImage
---
+++ build.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+export HERE=$(dirname "$(readlink -f "${0}")")
+
+if [[ -z "$1" ]] || [[ ! -d "$1" ]]; then
+    echo "You must specify a build destination folder"
+else
+    if [[ ! -d "$HERE/appimagetool" ]]; then
+        rm -rf "$HERE/squashfs-root" "$HERE/appimagetool.AppImage"
+        wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage \
+            -O "$HERE/appimagetool.AppImage"
+        chmod a+x "$HERE/appimagetool.AppImage"
+        "$HERE/appimagetool.AppImage" --appimage-extract
+        mv "$HERE/squashfs-root" "$HERE/appimagetool"
+        rm "$HERE/appimagetool.AppImage"
+    fi
+    dotnet run --project "$HERE" "$HERE" "$1"
+fi
