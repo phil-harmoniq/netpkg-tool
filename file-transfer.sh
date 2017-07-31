f19b25fb63d2cc34ed8e3dccf38eed396fed5134
Phil H <ph-hawkins@outlook.com>
2017-07-31 12:07:15 -0400

    New feature: netpkg.lib
     - Triggered if the source directory contains a netpkg.lib folder
     - All files (following symbolic links) will get coppied into $HERE/usr/lib
     - $LD_LIBRARY_PATH will be properly set to include the libraries

--- file-transfer.sh
+++ file-transfer.sh
@@ -13,14 +13,13 @@ set -e
 
 # Remove old temp folder & make new one
 rm -rf /tmp/"$APP_NAME".temp
-mkdir -p /tmp/"$APP_NAME".temp/usr/bin
 mkdir -p /tmp/"$APP_NAME".temp/usr/share/"$APP_NAME"
 
 # Transfer files from publish folder into temp folder
-if [[ -z $MAKE_SCD ]]; then
-    cp -r "$PROJECT"/bin/Release/"$CORE_VERS"/publish/. /tmp/"$APP_NAME".temp/usr/share/"$APP_NAME"
-else
+if [[ $MAKE_SCD == "true" ]]; then
     cp -r "$PROJECT"/bin/Release/"$CORE_VERS"/linux-x64/publish/. /tmp/"$APP_NAME".temp/usr/share/"$APP_NAME"
+else
+    cp -r "$PROJECT"/bin/Release/"$CORE_VERS"/publish/. /tmp/"$APP_NAME".temp/usr/share/"$APP_NAME"
 fi
 
 # Create an AppRun launcher
@@ -30,6 +29,13 @@ touch /tmp/"$APP_NAME".temp/AppRun
     echo
     echo 'export HERE=$(dirname "$(readlink -f "${0}")")'
     echo "export NET_PKG=\"$NET_PKG\""
+
+    if [[ -d $PROJECT/netpkg.lib ]] && [[ -z $LD_LIBRARY_PATH ]]; then
+        echo 'export LD_LIBRARY_PATH="$HERE/usr/lib"'
+    elif [[ -d $PROJECT/netpkg.lib ]]; then
+        echo 'export LD_LIBRARY_PATH="$HERE/usr/lib:$LD_LIBRARY_PATH"'
+    fi
+
     if [[ -z $MAKE_SCD ]]; then
         echo 'dotnet "$HERE/usr/share/'"$APP_NAME"'/'"$DLL_NAME.dll"'" "$@"'
     else
@@ -52,6 +58,12 @@ touch /tmp/"$APP_NAME".temp/"$APP_NAME"-icon.png
 
 # Set executable
 chmod +x /tmp/"$APP_NAME".temp/AppRun
+
+# Check for netpkg.lib and import libraries
+if [[ -d $PROJECT/netpkg.lib ]]; then
+    mkdir -p /tmp/"$APP_NAME".temp/usr/lib
+    cp -a -L "$PROJECT"/netpkg.lib/. /tmp/"$APP_NAME".temp/usr/lib/
+fi
 
 # Delete .NET Core debug databse files
 rm -f /tmp/"$APP_NAME".temp/usr/share/"$APP_NAME"/*.pdb
